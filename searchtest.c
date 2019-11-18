125376b29155861396d23f1c8752057f14a0cdfc
ccarnesi <cfc80@ice.cs.rutgers.edu>
2019-11-17 21:55:26 -0500

    added tests that run the search on different sizes

--- searchtest.c
+++ searchtest.c
@@ -9,16 +9,39 @@ float getAvgLong(long array[], int size);
 void getMetrics(long array[], int size, char* testName);
 
 void testA();
+void testB();
+void testC();
+void testD();
 
 int main(int argc, char* argv[]){
         testA();
+        testB();
+        testC();
+        testD();
 }
 
 void testA(){
     long timeArray[100];
     struct timeval start, end;
+    int* array = giveMeARandomArray(100000);
+    printf("Running Test A which searches a 100,000 elelement array for the number 15,200\n");    
+    int i = 0;
+    for(i=0;i<100;i++){
+            gettimeofday(&start, NULL);
+            int found = performSearch(array, 100000, 15200);
+            gettimeofday(&end, NULL);
+            long seconds = end.tv_sec - start.tv_sec;
+            timeArray[i] = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+            printf("Ran iteration %d which took %d ms\n", (i+1), timeArray[i]);
+            array = swapFoundWithRandom(array, found, 100000);
+    }
+    getMetrics(timeArray, 100, "Test A");
+}
+void testB(){
+    long timeArray[100];
+    struct timeval start, end;
     int* array = giveMeARandomArray(50000);
-    printf("Running Test A which searches a 50,000 elelement array for the number 15,200\n");    
+    printf("Running Test B which searches a 50,000 elelement array for the number 15,200\n");    
     int i = 0;
     for(i=0;i<100;i++){
             gettimeofday(&start, NULL);
@@ -29,11 +52,42 @@ void testA(){
             printf("Ran iteration %d which took %d ms\n", (i+1), timeArray[i]);
             array = swapFoundWithRandom(array, found, 50000);
     }
-    getMetrics(timeArray, 100, "Test A");
-
+    getMetrics(timeArray, 100, "Test B");
 }
-
-
+void testC(){
+    long timeArray[100];
+    struct timeval start, end;
+    int* array = giveMeARandomArray(10000);
+    printf("Running Test C which searches a 10,000 elelement array for the number 200\n");    
+    int i = 0;
+    for(i=0;i<100;i++){
+            gettimeofday(&start, NULL);
+            int found = performSearch(array, 10000, 200);
+            gettimeofday(&end, NULL);
+            long seconds = end.tv_sec - start.tv_sec;
+            timeArray[i] = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+            printf("Ran iteration %d which took %d ms\n", (i+1), timeArray[i]);
+            array = swapFoundWithRandom(array, found, 10000);
+    }
+    getMetrics(timeArray, 100, "Test C");
+}
+void testD(){
+    long timeArray[100];
+    struct timeval start, end;
+    int* array = giveMeARandomArray(1000);
+    printf("Running Test D which searches a 1,000 elelement array for the number 200\n");    
+    int i = 0;
+    for(i=0;i<100;i++){
+            gettimeofday(&start, NULL);
+            int found = performSearch(array, 1000, 200);
+            gettimeofday(&end, NULL);
+            long seconds = end.tv_sec - start.tv_sec;
+            timeArray[i] = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+            printf("Ran iteration %d which took %d ms\n", (i+1), timeArray[i]);
+            array = swapFoundWithRandom(array, found, 1000);
+    }
+    getMetrics(timeArray, 100, "Test D");
+}
 int* swapFoundWithRandom(int* array, int index, int size){
     int k = rand() % size;
     int temp = array[k];
